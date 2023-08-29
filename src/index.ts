a8229f46b9a4648d5a6a088d7691b92849b53cf8
Colson <colsoncalifornication@gmail.com>
2023-08-29 12:52:23 +0545

    configure route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('server');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Login Server</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
