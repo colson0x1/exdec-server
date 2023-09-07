910636e55274a2194e80c410f219420c18570786
Colson <colsoncalifornication@gmail.com>
2023-09-07 10:51:11 +0545

    test middleware

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -1,4 +1,13 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
@@ -8,19 +17,20 @@ function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
         /*
-          tsconfig.json
+          @ tsconfig.json
           "target": "es5"
         */
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
             var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.Path, target.prototype, key);
             var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.Method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.Middleware, target.prototype, key) || [];
             if (path) {
-                router[method]("".concat(routePrefix).concat(path), routeHandler);
+                router[method].apply(router, __spreadArray(__spreadArray(["".concat(routePrefix).concat(path)], middlewares, false), [routeHandler], false));
             }
         }
         /*
-          tsconfig.json
+          @ tsconfig.json
           "target": "es2016"
           NOTE: In ES2016, class methods are no longer enumerable
           Resolution for es2016
@@ -29,8 +39,10 @@ function controller(routePrefix) {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata('path', target.prototype, key);
     
+            ...
+    
             if (path) {
-              router.get(`${routePrefix}${path}`, routeHandler);
+              router.get(`${routePrefix}${path}`, ...middlewares, routeHandler);
             }
           });
         */
