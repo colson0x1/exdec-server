8015122781ffb665ef41fa30a8b897c47422e191
Colson <colsoncalifornication@gmail.com>
2023-09-07 14:38:54 +0545

    refactor express server configurations with Class based approach

--- build/index.js
+++ build/index.js
@@ -9,10 +9,32 @@ var cookie_session_1 = __importDefault(require("cookie-session"));
 var AppRouter_1 = require("./AppRouter");
 require("./controllers/LoginController");
 require("./controllers/RootController");
-var app = (0, express_1.default)();
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use((0, cookie_session_1.default)({ keys: ['lmao'] }));
-app.use(AppRouter_1.AppRouter.getInstance());
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
+/*
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['lmao'] }));
+app.use(AppRouter.getInstance());
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
 });
+*/
+/*
+@ Refractor express to work with TypeScript using Class based approach
+*/
+var Server = /** @class */ (function () {
+    function Server() {
+        this.app = (0, express_1.default)();
+        this.app.use(body_parser_1.default.urlencoded({ extended: true }));
+        this.app.use((0, cookie_session_1.default)({ keys: ['lmao'] }));
+        this.app.use(AppRouter_1.AppRouter.getInstance());
+    }
+    Server.prototype.start = function () {
+        this.app.listen(3000, function () {
+            console.log('Listening on port 3000');
+        });
+    };
+    return Server;
+}());
+new Server().start();
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
-import './controllers/RootController'; 
+import './controllers/RootController';
 
+/* 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,3 +15,25 @@ app.use(AppRouter.getInstance());
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+*/
+
+/*
+@ Refractor express to work with TypeScript using Class based approach
+*/
+class Server {
+  app: express.Express = express();
+
+  constructor() {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(cookieSession({ keys: ['lmao'] }));
+    this.app.use(AppRouter.getInstance());
+  }
+
+  start(): void {
+    this.app.listen(3000, () => {
+      console.log('Listening on port 3000');
+    });
+  }
+}
+
+new Server().start();
